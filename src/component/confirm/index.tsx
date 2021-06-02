48d03972ef5532601ce50d2645b35cd4be8a3a34
mengdaoshizhongxinyang <729403918@qq.com>
2021-06-02 17:58:16 +0800

    chore:confirm

--- src/component/confirm/index.tsx
+++ src/component/confirm/index.tsx
@@ -3,21 +3,34 @@
  * @Date: 2021-06-01 16:18:49
  * @Description: 
  */
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./confirm.module.less";
 import Modal from "@/component/modal";
 type Props = {
-  visible: boolean
+  visible?: boolean
   cancel?: () => any
+  confirm?: () => any
 }
 const confirm: React.FC<Props> = (props) => {
-  return <Modal visible={props.visible}>
+  const [visible, setVisible] = useState(false)
+  useEffect(() => {
+    setVisible(props.visible || false)
+  }, [props.visible])
+  function handleConfirm() {
+    props.cancel?.()
+    setVisible(false)
+  }
+  function handleCancel() {
+    props.confirm?.()
+    setVisible(false)
+  }
+  return <Modal visible={visible}>
     <div className={style['main']}>
       {props.children}
     </div>
     <div className={style['button']}>
-      <div className={style['cancel-button']}></div>
-      <div className={style['confirm-button']}></div>
+      <div className={style['cancel-button']}>取消</div>
+      <div className={style['confirm-button']}>确定</div>
     </div>
   </Modal>
 }
